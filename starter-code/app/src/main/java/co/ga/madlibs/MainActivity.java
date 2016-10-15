13ad773ed25c51ac710ab2c2e5c33022da382e9e
ScottLindley <scottdlindley@gmail.com>
2016-10-15 14:31:02 -0400

    Scott Lindley - activities and intents lab

--- starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
+++ starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
@@ -1,12 +1,68 @@
 package co.ga.madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mAdjective1;
+    private EditText mAdjective2;
+    private EditText mNoun1;
+    private EditText mNoun2;
+    private EditText mAnimals;
+    private EditText mGame;
+    private Button mSubmitButton;
+    private EditText[] mTextInputs = new EditText[6];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdjective1 = (EditText)findViewById(R.id.adjective1);
+        mAdjective2 = (EditText)findViewById(R.id.adjective2);
+        mNoun1 = (EditText)findViewById(R.id.noun1);
+        mNoun2 = (EditText)findViewById(R.id.noun2);
+        mAnimals = (EditText)findViewById(R.id.animals);
+        mGame = (EditText)findViewById(R.id.game);
+        mSubmitButton = (Button)findViewById(R.id.submit_button);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTextInputs[0] = mAdjective1;
+                mTextInputs[1] = mAdjective2;
+                mTextInputs[2] = mNoun1;
+                mTextInputs[3] = mNoun2;
+                mTextInputs[4] = mAnimals;
+                mTextInputs[5] = mGame;
+
+                for(EditText editText : mTextInputs){
+                    if(editText.getText().toString().equals("")){
+                        editText.setError("Please fill out!");
+                        return;
+                    }
+                }
+                /*
+                If the user input for 'Animal (plural)' doesn't end in an 's', remind the user
+                the input for this line must be pluralized.
+                 */
+                String animalsInput = mTextInputs[4].getText().toString();
+                if(!(animalsInput.charAt(animalsInput.length()-1)=='s')){
+                    mTextInputs[4].setError("Must be pluralized!");
+                    return;
+                }
+                String[] inputStrings = new String[6];
+                for(int i=0; i<mTextInputs.length; i++) {
+                    inputStrings[i] = mTextInputs[i].getText().toString().toLowerCase();
+                }
+                    Intent toResult = new Intent(MainActivity.this, ResultActivity.class);
+                    toResult.putExtra("INPUTS", inputStrings);
+                startActivity(toResult);
+            }
+        });
     }
 }
--- starter-code/app/src/main/java/co/ga/madlibs/ResultActivity.java
+++ starter-code/app/src/main/java/co/ga/madlibs/ResultActivity.java
@@ -1,24 +1,32 @@
 package co.ga.madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Created by james on 12/7/15.
  */
 public class ResultActivity extends AppCompatActivity {
     private Button mBackButton;
+    private TextView mStory;
+    private String mSentence1;
+    private String mSentence2;
+    private String mSentence3;
+    private String mSentence4;
+    private final String mVowels = "aeiou";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent receivedIntent = getIntent();
+        String[] inputs = receivedIntent.getStringArrayExtra("INPUTS");
 
         mBackButton = (Button) findViewById(R.id.back_button);
 
@@ -28,6 +36,37 @@ public class ResultActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        mStory = (TextView)findViewById(R.id.result_textview);
+
+
+
+
+
+        mSentence1 = "I went to the zoo today and saw a bunch of <b>"+inputs[0]+" "+inputs[4]+"</b>.";
+        /*Check to see if the inputs[1,2,3] begin with a vowel
+          If they do, then make the previous word 'an' instead of 'a'.
+         */
+        if(mVowels.contains(Character.toString(inputs[2].charAt(0)))){
+            mSentence2 = " The zookeepers had given them an <b>"+inputs[2]+"</b> to play with and " +
+                    "they were trying to play <b>"+inputs[5]+"</b> with it.";
+        }else{
+            mSentence2 = " The zookeepers had given them a <b>"+inputs[2]+"</b> to play with and " +
+                    "they were trying to play <b>"+inputs[5]+"</b> with it.";
+        }
+        if(mVowels.contains(Character.toString(inputs[3].charAt(0)))){
+            mSentence3 = " Afterwards, dad bought me an <b>"+inputs[3]+"</b> for being a good boy.";
+        }else{
+            mSentence3 = " Afterwards, dad bought me a <b>"+inputs[3]+"</b> for being a good boy.";
+        }
+        if(mVowels.contains(Character.toString(inputs[1].charAt(0)))){
+            mSentence4 = " What an <b>"+inputs[1]+"</b> day!";
+        }else {
+            mSentence4 = " What a <b>" + inputs[1] + "</b> day!";
+        }
+
+        //Now string the sentences together and set it to the TextView
+        mStory.setText(Html.fromHtml(mSentence1 + mSentence2 + mSentence3 + mSentence4));
 
     }
 }
